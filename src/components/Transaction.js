c1d58c362780acff053216c9965606bc6d6c65cb
danarkun <danarkun@gmail.com>
2020-10-13 13:54:54 +1030

    Names on transactions

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -6,9 +6,14 @@ import { GlobalContext } from '../context/GlobalState';
 export const Transaction = ({ transaction }) => {
     // Get reference to deletetransaction function from global state
     const { deleteTransaction } = useContext(GlobalContext);
+    const { userList } = useContext(GlobalContext);
+
+    // Get the first name of the user object with ID that matches the user id for this transaction
+    const { fname } = userList.find(x => x.id == transaction.user);
+
     return (
         <li className={transaction.amount > 0 ? "plus" : "minus"}>
-            {transaction.text} {transaction.user} <span>{transaction.amount > 0 ? "+" : "-"}${Math.abs(transaction.amount)}</span><button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
+            {transaction.text} {fname} <span>{transaction.amount > 0 ? "+" : "-"}${Math.abs(transaction.amount)}</span><button className="delete-btn" onClick={() => deleteTransaction(transaction.id)}>x</button>
         </li>
     )
 }
