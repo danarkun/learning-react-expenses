afeb08ff5a4ce3604bb91d64862077b982ff970a
danarkun <danarkun@gmail.com>
2020-10-15 11:30:48 +1030

    Add files via upload

--- src/components/TransactionViewer.js
+++ src/components/TransactionViewer.js
@@ -1,15 +1,38 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import { useLocation } from "react-router-dom";
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionViewer = () => {
     const location = useLocation();
-    useEffect(() => {
-        console.log(location.state.detail);
-    }, [location])
+    const [transaction, setTransaction] = useState(location.state.detail);
+    const { userList } = useContext(GlobalContext);
+    var purchaser;
+
+    if (userList.find(x => x.id == transaction.user) === undefined) {
+        purchaser = "deleted used";
+    }
+    else {
+        purchaser = userList.find(x => x.id == transaction.user).fname;
+    }
     
     return (
         <div>
             <h1>TRANSACTION VIEWER</h1>
+            <p>Transaction: {transaction.text}</p>
+            <p>Transaction Amount: {transaction.amount >= 0 ? "+" : "-"}${Math.abs(transaction.amount)}</p>
+            <p>Transaction ID: {transaction.id}</p>
+            <p>Transaction Name: {purchaser}</p>
         </div>
     )
 }
+
+
+    // useEffect(() => {
+    //     // Get transaction that we've been passed from clicked Transaction component
+    //     setTransaction(location.state.detail);
+
+    //     return() => {
+    //         console.log("CLEANING UP");
+    //     }
+    // }, [location])
