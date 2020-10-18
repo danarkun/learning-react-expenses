a3c1aec6cc25917baf4a8ae16482bec304525e82
danarkun <danarkun@gmail.com>
2020-10-19 10:14:23 +1030

    Add files via upload

--- src/components/UserViewer.js
+++ src/components/UserViewer.js
@@ -7,6 +7,7 @@ export const UserViewer = () => {
     const location = useLocation();
     const [user, setUser] = useState(location.state.detail);
     const { deleteUser } = useContext(GlobalContext);
+    const { transactions } = useContext(GlobalContext);
 
     const history = useHistory();
 
@@ -25,13 +26,28 @@ export const UserViewer = () => {
         })
     }
 
+    function UserExpenses() {
+        var sum = 0;
+        if (transactions == undefined) {
+            return 0;
+        }
+
+        transactions.map(t => {
+            if (t.user == user.id) {
+                sum += t.amount;
+            }
+        })
+        return sum;
+    }
+
     return (
         <div>
             <h1>USER VIEWER</h1>
             <br />
 
             <p><b>User Name: </b>{user.fname} {user.lname}</p>
-            <p> <b>User added at: </b>{user.timeStamp == undefined ? "Unknown time" : user.timeStamp.toString()}</p>
+            <p><b>User added at: </b>{user.timeStamp == undefined ? "Unknown time" : user.timeStamp.toString()}</p>
+            <p><b>User expenditure: </b>${UserExpenses()}</p>
             <button className="btn deleteButton" onClick={() => DeleteUser()}>DELETE USER</button>
         </div>
     )
